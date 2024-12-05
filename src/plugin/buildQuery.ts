ada489846626e7417e510ed2da1a113a52171c14
paulz <z0rge@bk.ru>
2024-12-05 10:43:09 +0300

    fix buildQuery.ts

--- src/plugin/buildQuery.ts
+++ src/plugin/buildQuery.ts
@@ -1,24 +1,15 @@
 import {
   buildQueryContext,
-  ensureIsArray,
   QueryFormData,
 } from '@superset-ui/core';
 
 export default function buildQuery(formData: QueryFormData) {
   return buildQueryContext(formData, {
     buildQuery: baseQueryObject => {
-      const { metrics, columns, groupby } = formData;
-      const metricsHandle = () =>
-        ensureIsArray(metrics).length === 0 &&
-        ensureIsArray(columns).length === 0 &&
-        ensureIsArray(groupby).length === 0
-          ? ['count']
-          : metrics;
-
       return [
         {
           ...baseQueryObject,
-          metrics: metricsHandle(),
+          metrics: ['count'],
         },
       ];
     },
